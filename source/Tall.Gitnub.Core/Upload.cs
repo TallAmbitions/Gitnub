86e1b770ef47e8f6c9146987609fe7377fb8bf23
Joe Wasson <jwasson+git@gmail.com>
2011-04-10 13:49:23 -0700

    Create Nant build.

--- source/Tall.Gitnub.Core/Upload.cs
+++ source/Tall.Gitnub.Core/Upload.cs
@@ -2,5 +2,8 @@
 // See included LICENSE for details.
 namespace Tall.Gitnub.Core
 {
+    /// <summary>
+    /// Class that uploads files to Github's download area.
+    /// </summary>
     public class Upload {}
 }
